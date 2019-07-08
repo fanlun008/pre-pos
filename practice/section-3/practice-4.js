3a3af151e90735a4bee7177cd4789276010c74e9
LIAN12 <ANDREW.LI@oocl.com>
2019-07-08 20:59:00 +0800

    update

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,54 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var BArr = objectB['value']
+  console.log(BArr);
+
+  var same = [];
+
+  collectionA.forEach((value, index, array) => {
+    var reg = /[a-z]+-[\d]/
+    if (reg.test(value)) {
+      console.log(value + "is a ---")
+      var splitVlue = value.split("-");
+      console.log(splitVlue[0], splitVlue[1])
+      for (let i = 0; i < splitVlue[1]; i++) {
+        if (same[splitVlue[0]] == undefined) {
+          same[splitVlue[0]] = 1;
+        } else {
+          same[splitVlue[0]]++;
+        }
+      }
+    } else {
+      if (same[value] == undefined) {
+        same[value] = 1;
+      } else {
+        same[value]++;
+      }
+    }
+  })
+
+  console.log(same)
+
+  var totalCount = [];
+  for (var item in same) {
+    let one = {
+      key: item,
+      count: same[item]
+    }
+    totalCount.push(one);
+  }
+
+  console.log(totalCount)
+
+  totalCount.forEach((value, index, array) => {
+    for (let i of BArr) {
+      if (i == value['key']){
+
+        value['count'] -= Math.floor(value['count'] / 3);
+      }
+    }
+  })
+
+  return totalCount;
 }
