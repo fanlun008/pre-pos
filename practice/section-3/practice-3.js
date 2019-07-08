e72bdce547bee547ee07f73d504eb15794d1e247
LIAN12 <ANDREW.LI@oocl.com>
2019-07-08 20:54:14 +0800

    update

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,41 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+  var BArr = objectB['value']
+  console.log(BArr);
+
+  var same = [];
+
+  collectionA.forEach((value, index, array) => {
+    if(same[value] == undefined) {
+      same[value] = 1;
+    } else {
+      same[value]++;
+    }
+  })
+
+  console.log(same)
+
+  var totalCount = [];
+  for (var item in same) {
+    let one = {
+      key: item,
+      count: same[item]
+    }
+    totalCount.push(one);
+  }
+
+  console.log(totalCount)
+
+  totalCount.forEach((value, index, array) => {
+    for (let i of BArr) {
+      if (i == value['key']){
+
+        value['count'] -= Math.floor(value['count'] / 3);
+      }
+    }
+  })
+
+  return totalCount;
 }
